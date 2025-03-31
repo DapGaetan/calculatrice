9aa7e1828ffd70a0edef9c7024d14f0cdd6ad560
Gaëtan <78353982+DapGaetan@users.noreply.github.com>
2025-03-31 12:35:21 +0200

    test: :test_tube: added new test for cypress

--- cypress/e2e/calculatrice.cy.ts
+++ cypress/e2e/calculatrice.cy.ts
@@ -1,4 +1,5 @@
 describe('Calculatrice', () => {
+
   it('Effectue une addition', () => {
     cy.visit('http://localhost:4200');
     cy.get('[data-cy="1"]').click();
@@ -7,4 +8,78 @@ describe('Calculatrice', () => {
     cy.get('[data-cy="egal"]').click();
     cy.get('[data-cy="result"]').should('have.text', '3');
   });
+
+  it('Effectue une soustraction', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('[data-cy="5"]').click();
+    cy.get('[data-cy="moins"]').click();
+    cy.get('[data-cy="3"]').click();
+    cy.get('[data-cy="egal"]').click();
+    cy.get('[data-cy="result"]').should('have.text', '2');
+  });
+
+  it('Effectue une multiplication', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('[data-cy="4"]').click();
+    cy.get('[data-cy="multiplication"]').click();
+    cy.get('[data-cy="6"]').click();
+    cy.get('[data-cy="egal"]').click();
+    cy.get('[data-cy="result"]').should('have.text', '24');
+  });
+
+  it('Effectue une division', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('[data-cy="9"]').click();
+    cy.get('[data-cy="division"]').click();
+    cy.get('[data-cy="3"]').click();
+    cy.get('[data-cy="egal"]').click();
+    cy.get('[data-cy="result"]').should('have.text', '3');
+  });
+
+  it('Effectue une division par zéro', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('[data-cy="7"]').click();
+    cy.get('[data-cy="division"]').click();
+    cy.get('[data-cy="0"]').click();
+    cy.get('[data-cy="egal"]').click();
+    cy.get('[data-cy="result"]').should('have.text', 'NaN');
+  });
+
+  it('Effectue une opération avec plusieurs opérateurs', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('[data-cy="8"]').click();
+    cy.get('[data-cy="plus"]').click();
+    cy.get('[data-cy="2"]').click();
+    cy.get('[data-cy="multiplication"]').click();
+    cy.get('[data-cy="3"]').click();
+    cy.get('[data-cy="egal"]').click();
+    cy.get('[data-cy="result"]').should('have.text', '30');
+  });
+
+  it('Effectue un test avec un seul chiffre', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('[data-cy="0"]').click();
+    cy.get('[data-cy="egal"]').click();
+    cy.get('[data-cy="result"]').should('have.text', '0');
+  });
+
+  it('Efface l\'écran avec le bouton C', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('[data-cy="5"]').click();
+    cy.get('[data-cy="plus"]').click();
+    cy.get('[data-cy="3"]').click();
+    cy.get('[data-cy="clear"]').click();
+    cy.get('[data-cy="result"]').should('have.text', '');
+  });
+
+  it('Effectue une addition avec un nombre négatif', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('[data-cy="moins"]').click();
+    cy.get('[data-cy="5"]').click();
+    cy.get('[data-cy="plus"]').click();
+    cy.get('[data-cy="3"]').click();
+    cy.get('[data-cy="egal"]').click();
+    cy.get('[data-cy="result"]').should('have.text', '-2');
+  });
+
 });
