c8f0eeea4dfb17224c4fb978fa20a95448495960
Gaëtan <78353982+DapGaetan@users.noreply.github.com>
2025-03-31 10:41:24 +0200

    feat & test: :sparkles: /🧪Added calculator component, modified cypress for e2e test

--- cypress/e2e/calculatrice.cy.ts
+++ cypress/e2e/calculatrice.cy.ts
@@ -1,15 +1,10 @@
 describe('Calculatrice', () => {
-  it('Effectue une addition', () => {
-     // allez sur l'app
+  it('Effectue une addition', () => {     // allez sur l'app
     cy.visit('http://localhost:4200');
-
-    // utiliser les touche du numpad
-    cy.get('[data-cy="input1"]').type('2'); 
-    cy.get('[data-cy="plus"]').click();
+    cy.get('[data-cy="input1"]').type('2');// utiliser les touche du numpad
+    cy.get('[data-cy="operateur"]').select('+');
     cy.get('[data-cy="input2"]').type('3');
     cy.get('[data-cy="egal"]').click();
-
-    // Vérifie le résultat
-    cy.get('[data-cy="result"]').should('have.text', '5');
+    cy.get('[data-cy="result"]').should('have.text', '5');// Vérifie le résultat
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CalculatriceComponent } from './components/calculatrice/calculatrice.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalculatriceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
